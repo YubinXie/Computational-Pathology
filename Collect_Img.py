4db8f8fbba3ca69dfe2c769d419430bfe79c662e
Yubin <yoobintse@gmail.com>
2017-08-01 17:43:34 -0400

    windows

--- Collect_Img.py
+++ Collect_Img.py
@@ -8,15 +8,27 @@ def main():
     locallabel()
 
 def label():
-    URLlink = "http://slides.mskcc.org/slides/pengy@mskcc.org/19;p;403769.svs/getLabelFileBMP"
-    files = cStringIO.StringIO(urllib.urlopen(URLlink).read())
-    img = Image.open(files)
-    w,l = img.size
-    print w,l
-    length=int(1280.0/float(w)*float(l))
-    print length
-    new_img=img.resize((1280,length))
-    plt.imsave("../Output/test.bmp",new_img)
+    with open ("../Sample_List.txt","r") as OpenSampleList:
+        number = 0
+        for line in OpenSampleList:
+            target = re.search('k;(\d+).svs',line)
+            if(target):
+                number+=1
+                SampleID = target.group(0)
+                print SampleID
+                URLlink=("http://slides.mskcc.org/slides/huangk@mskcc.org/19;" +target + "/getLabelFileBMP")
+                files = cStringIO.StringIO(urllib.urlopen(URLlink).read())
+                img = Image.open(files)
+                w,l = img.size
+                print w,l
+                length=int(1280.0/float(w)*float(l))
+                print length
+                new_img=img.resize((1280,length))
+                plt.imsave("../Output/" + target + ".bmp",new_img)
+            else:
+                print line, "not found"
+    #URLlink = "http://slides.mskcc.org/slides/pengy@mskcc.org/19;p;403769.svs/getLabelFileBMP"
+
 
 def locallabel():
 
@@ -33,7 +45,7 @@ def locallabel():
 
 def sample():
     OutputFolder = "RawInput/"
-    with open ("Sample_List.txt","r") as OpenSampleList:
+    with open ("../Sample_List.txt","r") as OpenSampleList:
         number = 0
         for line in OpenSampleList:
             target = re.search('(\d+)',line)
